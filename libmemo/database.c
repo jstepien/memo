0bae8af14524b7b50f1720009fa2a77c0a344301
Jan Stępień <jstepien@users.sourceforge.net>
2008-11-30 11:24:09 +0100

    Implemented memo_database_init.

--- libmemo/database.c
+++ libmemo/database.c
@@ -2,7 +2,7 @@
 
 int
 memo_database_load(memo_database *db, const char *filename) {
-	if( sqlite3_open(filename, db) ){
+	if ( sqlite3_open(filename, db) ) {
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(*db));
 		sqlite3_close(*db);
 		return 1;
@@ -11,7 +11,34 @@ memo_database_load(memo_database *db, const char *filename) {
 }
 
 int
+memo_database_execute(memo_database db, const char *query) {
+	sqlite3_stmt *stmt;
+	if ( sqlite3_prepare_v2(db, query, -1, &stmt, NULL) ) {
+		fprintf(stderr, "Error parsing SQL query: %s\n", sqlite3_errmsg(db));
+		return 1;
+	}
+	if ( sqlite3_step(stmt) != SQLITE_DONE ) {
+		fprintf(stderr, "Error executing statement: %s\n", sqlite3_errmsg(db));
+		return 1;
+	}
+	if ( sqlite3_finalize(stmt) ) {
+		fprintf(stderr, "Error finalising statement: %s\n", sqlite3_errmsg(db));
+		return 1;
+	}
+	return 0;
+}
+
+int
 memo_database_init(memo_database db) {
+	const char *words_query = "CREATE TABLE words (id integer, word text, "\
+			"positive_answers integer, negative_answers integer, "\
+			"PRIMARY KEY (id), UNIQUE (id, word) );";
+	const char *translations_query = "CREATE TABLE translations (id integer, "\
+			"word_id integer, translation_id integer, PRIMARY KEY (id), "\
+			"UNIQUE (id) );";
+	if ( memo_database_execute(db, words_query) ||
+			memo_database_execute(db, translations_query) )
+		return 1;
 	return 0;
 }
 
